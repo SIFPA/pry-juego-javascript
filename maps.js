ff2bec1348b4f3af8972376f4b9f0476a89e71ee
Pablo Sifuentes <pablosifuentescacha@gmail.com>
2023-04-09 17:51:34 -0500

    Agregando archivos game, index, maps y styles

---
+++ maps.js
@@ -0,0 +1,52 @@
+/* 
+*Reglas
+*El final de cada nivel  debe ser el inicio del siquiente
+*/
+
+const emojis={
+    '-': ' ',
+    '0': '🚪',
+    'X': '💣',
+    'PLAYER': '☠️',
+    'BOMB_COLLISION': '💥',
+    'GAME_OVER': '👎',
+    'WIN': '🏆',
+};
+
+const maps = [];
+maps.push(`
+    IXXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    OXXXXXXXXX
+`);
+maps.push(`
+    O--XXXXXXX
+    X--XXXXXXX
+    XX----XXXX
+    X--XX-XXXX
+    X-XXX--XXX
+    X-XXXX-XXX
+    XX--XX--XX
+    XX--XXX-XX
+    XXXX---IXX
+    XXXXXXXXXX
+`);
+maps.push(`
+    I-----XXXX
+    XXXXX-XXXX
+    XX----XXXX
+    XX-XXXXXXX
+    XX-----XXX
+    XXXXXX-XXX
+    XX-----XXX
+    XX-XXXXXXX
+    XX-----OXX
+    XXXXXXXXXX
+`);
